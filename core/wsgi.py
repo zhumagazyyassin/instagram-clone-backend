a1b5eeec38d52638abe71c65aa90a02d2b99c103
zhumagazyyassin <yassinzhumagazy13@gmail.com>
2026-04-17 19:45:40 +0500

    Update wsgi.py with path fix

--- core/wsgi.py
+++ core/wsgi.py
@@ -8,8 +8,15 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+# Жобаның негізгі папкасын Python жолына (sys.path) қосу
+# Бұл Render-де "No module named 'core'" қатесін болдырмауға көмектеседі
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
